23d15a2301f8a29625255746b1e9de2442ab6bbb
田世远 <tiansy@cyberpeace.cn>
2022-03-14 17:47:18 +0800

    表单更新

---
+++ src/views/user/form-inputs.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    type: 'input',
+    label: '姓名',
+    name: 'name',
+    width: 300,
+    maxlength: 30,
+    rule: [
+      { required: true, message: "请输入姓名", trigger: "blur" },
+      { min: 2, max: 30, message: "长度在 2 到 30 个字符", trigger: "blur" },
+    ]
+  },
+  {
+    type: 'options',
+    label: '性别',
+    name: 'gender',
+    value: '1',
+    kind: 2,
+    options: [{
+      label: '男',
+      value: '1'
+    }, {
+      label: '女',
+      value: '0'
+    }]
+  },
+  {
+    type: 'input',
+    label: '电话',
+    value: '189',
+    name: 'phone',
+    width: 300,
+    maxlength: 30,
+    rule: [
+      { validator(rule, value, callback) {
+        if (!value) {
+          callback(new Error('电话必填'))
+        } else if (!value.match(/^\d+$/)) {
+          callback(new Error('电话必须纯数字'))
+        } else {
+          callback()
+        }
+        
+      }, trigger: "blur" },
+    ]
+  },
+  {
+    type:'datepicker',
+    label: '生日',
+    name: 'birthday'
+  }
+]
--- src/views/user/user-edit-syform-index.vue
+++ src/views/user/user-edit-syform-index.vue
@@ -5,35 +5,10 @@
 </template>
 
 <script>
+import userFormInput from './form-inputs.js'
 export default {
   created() {
-    this.formInputs = [
-      {
-        type: 'input',
-        label: '姓名',
-        name: 'name',
-        width: 300,
-        maxlength: 30,
-        rule: [
-          { required: true, message: "请输入姓名", trigger: "blur" },
-          { min: 2, max: 30, message: "长度在 2 到 30 个字符", trigger: "blur" },
-        ]
-      },
-      {
-        type: 'options',
-        label: '性别',
-        name: 'gender',
-        value: '0',
-        kind: 2,
-        options: [{
-          label: '男',
-          value: '1'
-        }, {
-          label: '女',
-          value: '0'
-        }]
-      }
-    ]
+    this.formInputs = userFormInput
   },
   computed: {
     id() {
